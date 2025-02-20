05eb10e357408f2f4931aa92b0c1fe02d9d0393a
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-02-20 19:30:44 +0100

    more

---
+++ app/Filament/Resources/LibraryResource/Pages/CreateLibrary.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\LibraryResource\Pages;
+
+use App\Filament\Resources\LibraryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLibrary extends CreateRecord
+{
+    protected static string $resource = LibraryResource::class;
+}
